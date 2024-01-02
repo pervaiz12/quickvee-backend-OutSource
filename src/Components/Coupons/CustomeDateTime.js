4d4ce84889072d83b4e92fec0d3157567458d645
VikasMishra <vikas@imerchantech.com>
2024-01-02 11:13:26 +0530

    new coupon code

---
+++ src/Components/Coupons/CustomeDateTime.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+
+const CustomDateTime = () => {
+  // const [selectedStartDate, handleStartDateChange] = useState(null);
+  // const [selectedEndDate, handleEndDateChange] = useState(null);
+
+  return (
+    <>
+      <div style={{ display: "flex", gap: "1rem" }} className="date_selected">
+        <LocalizationProvider
+          dateAdapter={AdapterDayjs}
+          className="date-provider"
+        >
+          <DatePicker
+            label="Start date"
+         className="input_label_section"
+            renderInput={() => <input className="date-picker-input" />}
+          />
+          <div className="bl"></div>
+        </LocalizationProvider>
+        <div className="q_time_display">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <TimePicker label="Start Time"
+         className="input_label_section"
+          style={{ fontSize: '12px' }} />
+
+        </LocalizationProvider>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CustomDateTime;
