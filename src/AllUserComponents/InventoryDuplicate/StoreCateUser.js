b9f5aef072ac6102d1cab404ee9a4b69bcc72bec
Sachin <sachin@imerchantech.com>
2024-01-30 15:30:35 +0530

    duplicate inventory start

--- src/AllUserComponents/InventoryDuplicate/StoreCateUser.js
+++ src/AllUserComponents/InventoryDuplicate/StoreCateUser.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState ,useEffect} from "react";
+import { useDispatch } from "react-redux";
 import DownIcon from "../../Assests/Dashboard/Down.svg";
 import ReCAPTCHA from "./ReCAPTCHA";
+import {fetchMerchantsList} from "../../Redux/features/ExportInventory/ExportInventorySlice";
+import { useSelector } from "react-redux";
+import InventoryExportLogic from "./InventoryDuplicatLogic";
 
 const StoreCateUser = () => {
   const [selectedEmployee, setSelectedEmployee] = useState("All");
@@ -14,6 +19,29 @@ const StoreCateUser = () => {
     useState(false);
 
   const [replicateUPCs, setReplicateUPCs] = useState(false); // State for the checkbox
+
+  const {
+    handleStoreInput,
+    dupplicateInventory,
+    dupplicateSettings,
+    submitmessage,
+    setsubmitmessage,
+  } = InventoryExportLogic();
+
+  const [MerchantList, setMerchantList] = useState()
+  const MerchantListData = useSelector((state) => state.ExportInventoryData);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    if (!MerchantListData.loading && MerchantListData.MerchantListData) {
+        setMerchantList(MerchantListData.MerchantListData)
+        console.log(MerchantList)
+    }
+  }, [MerchantListData, MerchantListData.loading])
+
+  useEffect(() => {
+    dispatch(fetchMerchantsList())
+  }, [])
 
   const toggleDropdown = (dropdown) => {
     switch (dropdown) {
@@ -66,113 +94,69 @@ const StoreCateUser = () => {
             </span>
           </div>
 
-          <div className="q-order-page-container ml-8 md:flex-col">
-            {/* Employee Dropdown */}
-            <div className="q-order-page-filter mt-6">
-              <label className="q-details-page-label" htmlFor="employeeFilter">
-                Copy from this store
-              </label>
-              <div className="custom-dropdown">
-                <div
-                  className="custom-dropdown-header"
-                  onClick={() => toggleDropdown("employee")}
-                >
-                  <span className="selected-option mt-1">
-                    {selectedEmployee}
-                  </span>
-                  <img src={DownIcon} alt="Down Icon" className="w-8 h-8" />
-                </div>
-                {employeeDropdownVisible && (
-                  <div className="dropdown-content">
-                    <div onClick={() => handleOptionClick("All", "employee")}>
-                      All
-                    </div>
-                    <div
-                      onClick={() => handleOptionClick("employee1", "employee")}
-                    >
-                      employee1
-                    </div>
-                    <div
-                      onClick={() => handleOptionClick("employee2", "employee")}
-                    >
-                      employee2
-                    </div>
+            <div className="q-order-page-container ml-8 md:flex-col">
+              {/* Employee Dropdown */}
+              
+              <div className="col-qv-6">
+                  <div className="input_area">
+                      <label>Copy from this store</label>
+                      <select name="store_name_from" 
+                          // onChange={handleStoreInput}
+                      >
+                        <option value=''>--Select Store--</option>
+                      {MerchantList && MerchantList.length >= 1 && MerchantList.map(item => (
+                        <option key={item.id} value={item.merchant_id}>{item.name}</option>
+                        ))}
+                            
+                      </select>
                   </div>
-                )}
               </div>
-              <div className="mt-5">
-                <label
-                  className="q-details-page-label"
-                  htmlFor="orderSourceFilter"
-                >
-                  Paste to this store
+            </div>
+            <div className="q-order-page-container ml-8 md:flex-col">
+              <div className="col-qv-6">
+                  <div className="input_area">
+                      <label>Paste to this store</label>
+                      <select name="store_name_to" 
+                          // onChange={handleStoreInput}
+                      >
+                        <option value=''>--Select Store--</option>
+                      {MerchantList && MerchantList.length >= 1 && MerchantList.map(item => (
+                        <option key={item.id} value={item.merchant_id}>{item.name}</option>
+                        ))}
+                            
+                      </select>
+                  </div>
+              </div>
+            </div>
+              
+
+            <div className="q-add-inventory-section-header mx-2">
+              <div class="qv_checkbox">
+                <label class="qv_checkbox_add_checkmark_label">
+                  Want to Replicated UPC's for inventory
+                  <input
+                    type="checkbox"
+                    id="upc_check"
+                    name="upc_check"
+                    value="true"
+                    onChange={handleStoreInput}
+                  />
+                  <span class="qv_add_checkmark"></span>
                 </label>
-                <div className="custom-dropdown">
-                  <div
-                    className="custom-dropdown-header"
-                    onClick={() => toggleDropdown("orderSource")}
-                  >
-                    <span className="selected-option mt-1">
-                      {selectedOrderSource}
-                    </span>
-                    <img src={DownIcon} alt="Down Icon" className="w-8 h-8" />
-                  </div>
-                  {orderSourceDropdownVisible && (
-                    <div className="dropdown-content">
-                      <div
-                        onClick={() => handleOptionClick("All", "orderSource")}
-                      >
-                        All
-                      </div>
-                      <div
-                        onClick={() =>
-                          handleOptionClick("Source1", "orderSource")
-                        }
-                      >
-                        Source1
-                      </div>
-                      <div
-                        onClick={() =>
-                          handleOptionClick("Source2", "orderSource")
-                        }
-                      >
-                        Source2
-                      </div>
-                    </div>
-                  )}
-                </div>
               </div>
             </div>
 
-            <div className="q-order-page-filter mt-6"></div>
-          </div>
-
-          <div className="q-add-inventory-section-header mx-2">
-            <div class="qv_checkbox">
-              <label class="qv_checkbox_add_checkmark_label">
-                Want to Replicated UPC's for inventory
-                <input
-                  type="checkbox"
-                  id="inv_setting2"
-                  name="inv_setting_require"
-                  value="true"
-                />
-                <span class="qv_add_checkmark"></span>
-              </label>
+            <div
+              className="q-add-categories-section-middle-footer "
+              style={{ justifyContent: "start" }}
+            >
+              <button className="quic-btn quic-btn-save" onClick={dupplicateInventory} > 
+                Duplicate Inventory
+              </button>
+              <button className="quic-btn quic-btn-cancle" onClick={dupplicateSettings} >
+                Duplicate setting
+              </button>
             </div>
-          </div>
-
-          <div
-            className="q-add-categories-section-middle-footer "
-            style={{ justifyContent: "start" }}
-          >
-            <button className="quic-btn quic-btn-save">
-              Duplicate Inventory
-            </button>
-            <button className="quic-btn quic-btn-cancle">
-              Duplicate setting
-            </button>
-          </div>
         </div>
       </div>
     </>
