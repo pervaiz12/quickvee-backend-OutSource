0e27941149c015f4756908e1ddae66cf6a918693
Komal <komal@merchantech.com>
2024-02-01 16:35:51 +0530

    side menu updated

--- src/Components/Layout/SideMenu.js
+++ src/Components/Layout/SideMenu.js
@@ -124,15 +124,16 @@ const DropdownMenuItem = ({ item, isMenuOpen }) => {
   };
 
   return (
-    <div className="relative" style={{ width: isMenuOpen ? "16rem" : "6rem" }}>
+    <div className="relative" style={isMenuOpen ? { width: "16rem" } : { width: "6rem", marginLeft: "24px" }} >
 
     <div className="flex items-center">
+    {item.icon}
       {isMenuOpen && (
         <p
-          className="ml-2 cursor-pointer menu-item text-gray-400"
+          className="cursor-pointer menu-item text-gray-400"
           onClick={handleToggleDropdown}
         >
-          {item.icon}
+        
         </p>
       )}
   
@@ -148,7 +149,7 @@ const DropdownMenuItem = ({ item, isMenuOpen }) => {
     </div>
   
     {isDropdownOpen && (
-      <div className="absolute mt-0 bg-[#334247] p-4 shadow w-full text-center z-10">
+      <div className="mt-0 bg-[#334247] p-4 shadow w-full text-center z-10">
         {item.dropdownItems.map((dropdownItem) => (
           <Link
             key={dropdownItem.id}
