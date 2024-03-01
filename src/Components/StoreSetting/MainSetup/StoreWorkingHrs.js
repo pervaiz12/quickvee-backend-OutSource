8aaa0deb84d8fab31c72cef057cc4f90423f23fe
QuickveeAdmin <priya@merchantech.com>
2024-03-01 11:45:46 +0530

    shift api calling

--- src/Components/StoreSetting/MainSetup/StoreWorkingHrs.js
+++ src/Components/StoreSetting/MainSetup/StoreWorkingHrs.js
@@ -4,173 +4,437 @@ import DeleteIcon from "../../../Assests/Filter/DeleteSetup.svg";
 import AddIcon from "../../../Assests/Filter/AddSetup.svg";
 
 const StoreWorkingHrs = () => {
+  // State and handlers for Sunday
+  const [sundayOpen, setSundayOpen] = useState(false);
+  const [sundayStartTime, setSundayStartTime] = useState("");
+  const [sundayEndTime, setSundayEndTime] = useState("");
 
-  const [newDayAdded, setnewDayAdded] = useState(false)
-  // State and handlers for each day
-  const [days, setDays] = useState([
-    {
-      name: "Sunday",
-      open: true,
-      startTime: "",
-      endTime: ""
-    },
-    {
-      name: "Monday",
-      open: true,
-      startTime: "",
-      endTime: ""
-    },
-    {
-      name: "Tuesday",
-      open: true,
-      startTime: "",
-      endTime: ""
-    },
-    {
-      name: "Wednesday",
-      open: true,
-      startTime: "",
-      endTime: ""
-    },
-    {
-      name: "Thursday",
-      open: false,
-      startTime: "",
-      endTime: ""
-    },
-    {
-      name: "Friday",
-      open: false,
-      startTime: "",
-      endTime: ""
-    },
-    {
-      name: "Saturday",
-      open: false,
-      startTime: "",
-      endTime: ""
-    },
-   
-
-  ]);
-
-  const handleSwitchChange = (index) => {
-    setDays((prevDays) => {
-      const updatedDays = [...prevDays];
-      updatedDays[index].open = !updatedDays[index].open;
-      return updatedDays;
-    });
+  const handleSundaySwitchChange = () => {
+    setSundayOpen((prev) => !prev);
   };
 
-  const handleStartTimeChange = (index, e) => {
-    const { value } = e.target;
-    setDays((prevDays) => {
-      const updatedDays = [...prevDays];
-      updatedDays[index].startTime = value;
-      return updatedDays;
-    });
+  const handleSundayStartTimeChange = (e) => {
+    setSundayStartTime(e.target.value);
   };
 
-  const handleEndTimeChange = (index, e) => {
-    const { value } = e.target;
-    setDays((prevDays) => {
-      const updatedDays = [...prevDays];
-      updatedDays[index].endTime = value;
-      return updatedDays;
-    });
+  const handleSundayEndTimeChange = (e) => {
+    setSundayEndTime(e.target.value);
   };
 
-  const handleDeleteDay = (index , itemtype) => {
-    if(itemtype === "new"){
-      setDays((prevDays) => {
-        const updatedDays = [...prevDays];
-        updatedDays.splice(index, 1);
-        return updatedDays;
-      });
-    }
-    else{
-      setDays((prevDays) => {
-        const updatedDays = [...prevDays];
-        updatedDays[index].open = "Closed";
-        return updatedDays;
-      });
-    }
+  // State and handlers for Monday
+  const [mondayOpen, setMondayOpen] = useState(false);
+  const [mondayStartTime, setMondayStartTime] = useState("");
+  const [mondayEndTime, setMondayEndTime] = useState("");
+
+  const handleMondaySwitchChange = () => {
+    setMondayOpen((prev) => !prev);
   };
 
+  const handleMondayStartTimeChange = (e) => {
+    setMondayStartTime(e.target.value);
+  };
+
+  const handleMondayEndTimeChange = (e) => {
+    setMondayEndTime(e.target.value);
+  };
+  
+  // Function to delete a day
+  const handleDeleteDay = (day) => {
+    // Implement deletion logic here
+    console.log(`Deleting ${day}`);
+  };
+
+  // Function to add a day
   const handleAddDay = () => {
-    setnewDayAdded(true)
-    const newDay = {
-      name: "",
-      open: false,
-      startTime: "",
-      endTime: ""
-    };
-    setDays((prevDays) => [...prevDays, newDay]);
+    // Implement addition logic here
+    console.log("Adding a day");
   };
 
   return (
+    <>
     <div className="box_shadow_div">
       <div className="my-3">
-        <h5 className="box_shadow_heading" style={{ padding: "15px 30px" }}>
-          Store Working Hours
-        </h5>
+        <div className=""> 
+        <h5 className="box_shadow_heading" style={{padding:"15px 30px"}}>Store Working Hours</h5></div>
+       
 
-        {days.map((day, index) => (
-          <div key={index} className={`flex day-container ${index % 2 === 0 ? 'even' : 'odd'}`}>
-            <div style={{ width: "15%" }}>{day.name}</div>
-            <div style={{ width: "15%" }}>
-              <Switch
-                checked={day.open}
-                onChange={() => handleSwitchChange(index)}
+        {/* Sunday */}
+        <div className="flex day-container odd">
+          <div style={{ width: "15%" }}>Sunday</div>
+          <div style={{ width: "15%" }}>
+            <Switch
+              name="sunday_open"
+              checked={sundayOpen}
+              onChange={handleSundaySwitchChange}
+            />
+            {sundayOpen ? "Open" : "Closed"}
+          </div>
+          <div className="flex" style={{ width: "45%" }}>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_start_time"
+                value={sundayStartTime}
+                id="sunday_start_tym"
+                required
+                onChange={handleSundayStartTimeChange}
               />
-              {day.open ? "Open" : "Closed"}
             </div>
-            <div className="flex" style={{ width: "45%" }}>
-              <div className="input_area">
-                <input
-                  type="time"
-                  value={day.startTime}
-                  onChange={(e) => handleStartTimeChange(index, e)}
-                  disabled={!day.open}
-                />
-              </div>
-              <div className="q_store_working_section">
-                <div className="text-center">To</div>
-              </div>
-              <div className="input_area">
-                <input
-                  type="time"
-                  value={day.endTime}
-                  onChange={(e) => handleEndTimeChange(index, e)}
-                  disabled={!day.open}
-                />
-              </div>
+            <div className="q_store_working_section">
+              <div className="text-center">To</div>
             </div>
-            <div style={{ width: "5%" }}>
-
-              
-              <div className="flex justify-between">
-                <img
-                  src={AddIcon}
-                  alt=""
-                  className="ml-6 mt-2"
-                  onClick={() => handleDeleteDay(index, index === days.length - 1 && newDayAdded  ? "new" : "old")}
-                />
-                {index === days.length - 1 && (
-                  <img
-                    src={DeleteIcon}
-                    alt=""
-                    className="ml-6 mt-2"
-                    onClick={handleAddDay}
-                  />
-                )}
-              </div>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_end_time"
+                value={sundayEndTime}
+                id="sunday_end_tym"
+                required
+                onChange={handleSundayEndTimeChange}
+              />
             </div>
           </div>
-        ))}
+          <div style={{ width: "5%" }}>
+            <div className="flex justify-between">
+              <img
+                src={DeleteIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={() => handleDeleteDay("sunday")}
+              />
+              <img
+                src={AddIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={handleAddDay}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Monday */}
+        <div className="flex day-container even">
+          <div style={{ width: "15%" }}>Monday</div>
+          <div style={{ width: "15%" }}>
+            <Switch
+              name="monday_open"
+              checked={mondayOpen}
+              onChange={handleMondaySwitchChange}
+            />
+            {mondayOpen ? "Open" : "Closed"}
+          </div>
+          <div className="flex" style={{ width: "45%" }}>
+            <div className="input_area">
+              <input
+                type="time"
+                name="monday_start_time"
+                value={mondayStartTime}
+                id="monday_start_tym"
+                required
+                onChange={handleMondayStartTimeChange}
+              />
+            </div>
+            <div className="q_store_working_section">
+              <div className="text-center">To</div>
+            </div>
+            <div className="input_area">
+              <input
+                type="time"
+                name="monday_end_time"
+                value={mondayEndTime}
+                id="monday_end_tym"
+                required
+                onChange={handleMondayEndTimeChange}
+              />
+            </div>
+          </div>
+          <div style={{ width: "5%" }}>
+            <div className="flex justify-between">
+              <img
+                src={DeleteIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={() => handleDeleteDay("monday")}
+              />
+              <img
+                src={AddIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={handleAddDay}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex day-container odd">
+          <div style={{ width: "15%" }}>Tuesday</div>
+          <div style={{ width: "15%" }}>
+            <Switch
+              name="sunday_open"
+              checked={sundayOpen}
+              onChange={handleSundaySwitchChange}
+            />
+            {sundayOpen ? "Open" : "Closed"}
+          </div>
+          <div className="flex" style={{ width: "45%" }}>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_start_time"
+                value={sundayStartTime}
+                id="sunday_start_tym"
+                required
+                onChange={handleSundayStartTimeChange}
+              />
+            </div>
+            <div className="q_store_working_section">
+              <div className="text-center">To</div>
+            </div>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_end_time"
+                value={sundayEndTime}
+                id="sunday_end_tym"
+                required
+                onChange={handleSundayEndTimeChange}
+              />
+            </div>
+          </div>
+          <div style={{ width: "5%" }}>
+            <div className="flex justify-between">
+              <img
+                src={DeleteIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={() => handleDeleteDay("sunday")}
+              />
+              <img
+                src={AddIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={handleAddDay}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex day-container even">
+          <div style={{ width: "15%" }}>Wednesday</div>
+          <div style={{ width: "15%" }}>
+            <Switch
+              name="monday_open"
+              checked={mondayOpen}
+              onChange={handleMondaySwitchChange}
+            />
+            {mondayOpen ? "Open" : "Closed"}
+          </div>
+          <div className="flex" style={{ width: "45%" }}>
+            <div className="input_area">
+              <input
+                type="time"
+                name="monday_start_time"
+                value={mondayStartTime}
+                id="monday_start_tym"
+                required
+                onChange={handleMondayStartTimeChange}
+              />
+            </div>
+            <div className="q_store_working_section">
+              <div className="text-center">To</div>
+            </div>
+            <div className="input_area">
+              <input
+                type="time"
+                name="monday_end_time"
+                value={mondayEndTime}
+                id="monday_end_tym"
+                required
+                onChange={handleMondayEndTimeChange}
+              />
+            </div>
+          </div>
+          <div style={{ width: "5%" }}>
+            <div className="flex justify-between">
+              <img
+                src={DeleteIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={() => handleDeleteDay("monday")}
+              />
+              <img
+                src={AddIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={handleAddDay}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex day-container odd">
+          <div style={{ width: "15%" }}>Thursday</div>
+          <div style={{ width: "15%" }}>
+            <Switch
+              name="sunday_open"
+              checked={sundayOpen}
+              onChange={handleSundaySwitchChange}
+            />
+            {sundayOpen ? "Open" : "Closed"}
+          </div>
+          <div className="flex" style={{ width: "45%" }}>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_start_time"
+                value={sundayStartTime}
+                id="sunday_start_tym"
+                required
+                onChange={handleSundayStartTimeChange}
+              />
+            </div>
+            <div className="q_store_working_section">
+              <div className="text-center">To</div>
+            </div>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_end_time"
+                value={sundayEndTime}
+                id="sunday_end_tym"
+                required
+                onChange={handleSundayEndTimeChange}
+              />
+            </div>
+          </div>
+          <div style={{ width: "5%" }}>
+            <div className="flex justify-between">
+              <img
+                src={DeleteIcon}
+                alt=""
+                className="ml-6 mt-2 d-none"
+                onClick={() => handleDeleteDay("sunday")}
+              />
+              <img
+                src={AddIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={handleAddDay}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex day-container even">
+          <div style={{ width: "15%" }}>Friday</div>
+          <div style={{ width: "15%" }}>
+            <Switch
+              name="monday_open"
+              checked={mondayOpen}
+              onChange={handleMondaySwitchChange}
+            />
+            {mondayOpen ? "Open" : "Closed"}
+          </div>
+          <div className="flex" style={{ width: "45%" }}>
+            <div className="input_area">
+              <input
+                type="time"
+                name="monday_start_time"
+                value={mondayStartTime}
+                id="monday_start_tym"
+                required
+                onChange={handleMondayStartTimeChange}
+              />
+            </div>
+            <div className="q_store_working_section">
+              <div className="text-center">To</div>
+            </div>
+            <div className="input_area">
+              <input
+                type="time"
+                name="monday_end_time"
+                value={mondayEndTime}
+                id="monday_end_tym"
+                required
+                onChange={handleMondayEndTimeChange}
+              />
+            </div>
+          </div>
+          <div style={{ width: "5%" }}>
+            <div className="flex justify-between">
+              <img
+                src={DeleteIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={() => handleDeleteDay("monday")}
+              />
+              <img
+                src={AddIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={handleAddDay}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex day-container odd">
+          <div style={{ width: "15%" }}>Saturday</div>
+          <div style={{ width: "15%" }}>
+            <Switch
+              name="sunday_open"
+              checked={sundayOpen}
+              onChange={handleSundaySwitchChange}
+            />
+            {sundayOpen ? "Open" : "Closed"}
+          </div>
+          <div className="flex" style={{ width: "45%" }}>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_start_time"
+                value={sundayStartTime}
+                id="sunday_start_tym"
+                required
+                onChange={handleSundayStartTimeChange}
+              />
+            </div>
+            <div className="q_store_working_section">
+              <div className="text-center">To</div>
+            </div>
+            <div className="input_area">
+              <input
+                type="time"
+                name="sunday_end_time"
+                value={sundayEndTime}
+                id="sunday_end_tym"
+                required
+                onChange={handleSundayEndTimeChange}
+              />
+            </div>
+          </div>
+          <div style={{ width: "5%" }}>
+            <div className="flex justify-between">
+              <img
+                src={DeleteIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={() => handleDeleteDay("sunday")}
+              />
+              <img
+                src={AddIcon}
+                alt=""
+                className="ml-6 mt-2"
+                onClick={handleAddDay}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Repeat the structure for other days */}
+        {/* Tuesday, Wednesday, Thursday, Friday, Saturday */}
       </div>
-    </div>
+      </div>
+    </>
   );
 };
 
 export default StoreWorkingHrs;
+
+
+
