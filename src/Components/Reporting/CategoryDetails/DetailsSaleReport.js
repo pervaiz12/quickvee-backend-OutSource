8138bba76c3626e6396b9f24f26036b2dae5e059
SumeshMerchant <sumesh@merchantech.com>
2024-03-12 14:19:44 +0530

    Category Sales Report Done

--- src/Components/Reporting/CategoryDetails/DetailsSaleReport.js
+++ src/Components/Reporting/CategoryDetails/DetailsSaleReport.js
@@ -9,17 +9,20 @@ const DetailsSaleReport = ({ data }) => {
   const dispatch = useDispatch();
 
   const [detailCategorySale, setdetailCategorySale] = useState([]);
-  const [order, setOrder] = useState('DESC'); 
-  const [sorting_type, setSorting_type] = useState('categoryTotal'); 
+  const [order, setOrder] = useState("DESC");
+  const [sorting_type, setSorting_type] = useState("categoryTotal");
 
   const detailCategorySaleDataState = useSelector(
     (state) => state.detailCategorySale
   );
 
   useEffect(() => {
-   
-    dispatch(fetchdetailCategorySaleData({ ...data, order ,sorting_type})); 
-  }, [dispatch, data, order,sorting_type]);
+    if (!data.merchant_id) {
+      console.log("empty");
+    } else {
+      dispatch(fetchdetailCategorySaleData({ ...data, order, sorting_type }));
+    }
+  }, [dispatch, data, order, sorting_type]);
 
   useEffect(() => {
     if (
@@ -46,34 +49,36 @@ const DetailsSaleReport = ({ data }) => {
       }, 0)
     : 0;
 
+  const handleCategoryClick = () => {
+    // Toggle between ASC and DESC orders
+    const newSort = "categoryTotal";
+    const newOrder = order === "ASC" ? "DESC" : "ASC";
+    setOrder(newOrder);
+    setSorting_type(newSort);
+  };
 
-    const handleCategoryClick = () => {
-      // Toggle between ASC and DESC orders
-      const newSort = 'categoryTotal';
-      const newOrder = order === 'ASC' ? 'DESC' : 'ASC';
-      setOrder(newOrder);
-      setSorting_type(newSort);
-    };
-    
-    const handleQuantityClick = () => {
-      const newSort = 'productQuantity';
-      const newOrder = order === 'ASC' ? 'DESC' : 'ASC';
-      setSorting_type(newSort);
-      setOrder(newOrder);
-    };
+  const handleQuantityClick = () => {
+    const newSort = "productQuantity";
+    const newOrder = order === "ASC" ? "DESC" : "ASC";
+    setSorting_type(newSort);
+    setOrder(newOrder);
+  };
 
   return (
     <>
       {Object.entries(detailCategorySale).map(([category, items]) => (
         <div className="box" key={category}>
           <div className="q-attributes-bottom-detail-section ">
-            <div className="mt-6 "  >
-              <div className="q-attributes-bottom-header bg-[#ffffff] cursor-pointer"  onClick={handleCategoryClick}>
-                <span>{category}</span><img src={SortIcon} alt="" className="" />
+            <div className="mt-6 ">
+              <div
+                className="q-attributes-bottom-header bg-[#ffffff] cursor-pointer"
+                onClick={handleCategoryClick}
+              >
+                <span>{category}</span>
+                <img src={SortIcon} alt="" className="" />
               </div>
               <style>
-                {
-                  `.q-catereport-quantity,.attriButes-title{
+                {`.q-catereport-quantity,.attriButes-title{
                     display: flex !important;
                     cursor:pointer;
                   }
@@ -81,13 +86,25 @@ const DetailsSaleReport = ({ data }) => {
                     padding-left: 0.75rem;
                   }
                   
-                  `
-                }
+                  `}
               </style>
               <div className="q-attributes-bottom-attriButes-header">
                 <p className="q-catereport-item">Item Name</p>
-                <p className="q-catereport-quantity "   onClick={handleQuantityClick}>Quantity <span><img src={SortIconW} alt="" className="" /></span> </p>
-                <p className="attriButes-title" onClick={handleQuantityClick} >Amount <span><img src={SortIconW} alt="" className="" /></span></p>
+                <p
+                  className="q-catereport-quantity "
+                  onClick={handleQuantityClick}
+                >
+                  Quantity{" "}
+                  <span>
+                    <img src={SortIconW} alt="" className="" />
+                  </span>{" "}
+                </p>
+                <p className="attriButes-title" onClick={handleQuantityClick}>
+                  Amount{" "}
+                  <span>
+                    <img src={SortIconW} alt="" className="" />
+                  </span>
+                </p>
               </div>
               {items.map((item, index) => (
                 <div
@@ -98,7 +115,10 @@ const DetailsSaleReport = ({ data }) => {
                     <p className="q-catereport-item">{item.name}</p>
                     <p className="q-catereport-quantity ">{item.pro_qty}</p>
                     <p className="q-catereport-amount">
-                    ${item.product_total ? parseFloat(item.product_total).toFixed(2) : '0.00'}
+                      $
+                      {item.product_total
+                        ? parseFloat(item.product_total).toFixed(2)
+                        : "0.00"}
                     </p>
                   </div>
                 </div>
@@ -108,7 +128,8 @@ const DetailsSaleReport = ({ data }) => {
                   <p className="q-catereport-item">Total</p>
                   <p className="q-catereport-quantity"></p>
                   <p className="q-catereport-amount">
-                    ${items
+                    $
+                    {items
                       .reduce(
                         (acc, item) => acc + parseFloat(item.product_total),
                         0
