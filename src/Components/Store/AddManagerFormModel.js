4c4cba37fa321bbfd73899f16b03fb9ac02604ab
AjinkyaBansode <ajinkya@merchantech.com>
2024-05-14 10:40:26 +0530

    add Maneger page mdel added

---
+++ src/Components/Store/AddManagerFormModel.js
@@ -0,0 +1,152 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import AddIcon from "../../Assests/Category/addIcon.svg";
+import { FormControl, Grid } from "@mui/material";
+import BasicTextFields from "../../reuseableComponents/TextInputField";
+import SearchableDropdown from "../Products/SearchableDropdown";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 1000,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+const FormInputFields = {
+  "First Name": "",
+  "Last Name": "",
+  "Mobile Name": null,
+  "Email Address": "",
+  Password: "",
+  stores: ["Store Name-1", "Store Name-Lorem 2", "Store 3"],
+};
+const AddManagerFormModel = (props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [selectedOption, setSelectedOption] = useState([]);
+  const [formInputs, setFormInputs] = useState(FormInputFields);
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSelectedOptions =(value, name)=>{
+      setSelectedOption((prev)=> [...prev, value])
+  }
+
+  const handleDeleteSelectedOption=(id, name)=>{
+    // setSelectedOption(()
+    // console.log(name)
+    const filtervalue = selectedOption?.filter((opt)=>{
+      return opt?.id !== id
+    })
+    setSelectedOption(filtervalue)
+  }
+  return (
+    <>
+      <Button onClick={handleOpen}>
+        <p
+          className="me-3 select-none managerStore-btn"
+          style={{ whiteSpace: "nowrap" }}
+        >
+          Add Manager
+        </p>
+        <img src={AddIcon} />
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Grid container>
+            <Grid container>
+              <Grid item>
+                <p
+                  className="me-3 select-none Add-manager-forrm-title"
+                  style={{ whiteSpace: "nowrap" }}
+                >
+                  Add Employee
+                </p>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <label>First Name</label>
+                <BasicTextFields
+                  name="First Name"
+                  value={formInputs["First Name"]}
+                  onChangeFun={onChangeHandler}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <label>Last Name</label>
+                <BasicTextFields
+                  name="Last Name"
+                  value={formInputs["Last Name"]}
+                  onChangeFun={onChangeHandler}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <label>Mobile Name</label>
+                <BasicTextFields
+                  type={"number"}
+                  name="Mobile Name"
+                  value={formInputs["Mobile Name"]}
+                  onChangeFun={onChangeHandler}
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <label>Email Address</label>
+                <BasicTextFields
+                  name="Email Address"
+                  value={formInputs["Email Address"]}
+                  onChangeFun={onChangeHandler}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <label>Password</label>
+                <BasicTextFields
+                  name="Password"
+                  value={formInputs["Password"]}
+                  onChangeFun={onChangeHandler}
+                />
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item xs={12}>
+                <div className="">
+                  <SearchableDropdown 
+                     keyName="stores"
+                     optionList={props.stores}
+                     handleSelectProductOptions={handleSelectedOptions}
+                     selectedOption={selectedOption}
+                     handleDeleteSelectedOption={handleDeleteSelectedOption}
+                    //  selectedOption={productInfo?.relatedProduct}
+                    //  error={error}
+                     // handleUpdateError={handleUpdateError}
+                  />
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
+export default AddManagerFormModel;
--- src/Components/Store/ManagerStore.js
+++ src/Components/Store/ManagerStore.js
@@ -2,6 +2,8 @@ import { Grid } from "@mui/material";
 import storeDefaultImage from "../../Assests/Vendors/DefaultStore.svg";
 import EmailLogo from "../../Assests/Vendors/EmailLogo.svg";
 import PhoneLogo from "../../Assests/Vendors/PhoneLogo.svg";
+import AddIcon from "../../Assests/Category/addIcon.svg";
+import AddManagerFormModel from "./AddManagerFormModel";
 const managerStore = [
   {
     name: "Adam Willson",
@@ -35,10 +37,25 @@ const managerStore = [
   },
 ];
 
+const stores = [
+  { id: 1,value: 'chocolate', title: 'Chocolate' },
+  { id: 2,value: 'strawberry', title: 'Strawberry' },
+  { id: 3,value: 'vanilla', title: 'Vanilla' },
+];
 const ManagerStore = () => {
-  console.log(managerStore);
   return (
     <>
+      <Grid container-fluid className="managerStore-title-main border">
+        <Grid item xs={12}>
+          <div className="flex justify-between  m-3">
+            <p className="managerStore-title select-none">Manager</p>
+            <div className="flex items-center cursor-pointer">
+              {/* <p className="me-3 select-none managerStore-btn" style={{whiteSpace:"nowrap"}}>Add Manager</p> <img src={AddIcon} /> */}
+              <AddManagerFormModel stores={stores} />
+            </div>
+          </div>
+        </Grid>
+      </Grid>
       <Grid container className="store-items-list" spacing={2}>
         {managerStore.map((item, Index) => (
           <Grid item className="store-items " xs={12} sm={6} key={Index}>
@@ -49,8 +66,8 @@ const ManagerStore = () => {
                 </div>
                 <div className="grid content-center w-full store-items-address">
                   <div className="flex justify-between">
-                    <p className="store-items-store-name">{item.name}</p>
-                    <p className="store-items-edit-btn">Edit</p>
+                    <p className="store-items-store-name ">{item.name}</p>
+                    <p className="store-items-edit-btn select-none">Edit</p>
                   </div>
 
                   <div className="store-items-store-name-address w-full">
@@ -64,14 +81,14 @@ const ManagerStore = () => {
                     </span>
                   </div>
                   <div className="flex mt-2">
-                {item.stores.map((store) => (
-                  <p className="p-1 border me-3 store-items-store-names">{store}</p>
-                ))}
-              </div>
+                    {item.stores.map((store) => (
+                      <p className="p-1 border me-3 store-items-store-names">
+                        {store}
+                      </p>
+                    ))}
+                  </div>
                 </div>
               </div>
-
-              
             </div>
           </Grid>
         ))}
