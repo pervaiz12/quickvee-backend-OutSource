eb7c2e2894ec4e409b28cf1ec8bb549cf572c465
AjinkyaBansode <ajinkya@merchantech.com>
2024-05-23 11:35:44 +0530

    verified and unverified table and pagination

--- src/AllUserComponents/Users/UnverifeDetails/Pagination.js
+++ src/AllUserComponents/Users/UnverifeDetails/Pagination.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import LeftArrow from "../../../Assests/VerifiedMerchant/LeftArrow.svg"
-const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => {
+import React from "react";
+import LeftArrow from "../../../Assests/VerifiedMerchant/LeftArrow.svg";
+import RightArrow from "../../../Assests/VerifiedMerchant/RightArrow.svg";
+import SelectDropDown from "../../../reuseableComponents/SelectDropDown";
+const Pagination = ({
+  currentPage,
+  totalItems,
+  itemsPerPage,
+  onPageChange,
+  rowsPerPage,
+  setRowsPerPage,
+}) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   const renderPaginationItems = () => {
@@ -8,8 +17,14 @@ const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => 
 
     const addPageNumber = (pageNumber) => {
       items.push(
-        <li key={pageNumber} className={`page-item ${currentPage === pageNumber ? 'active' : ''}`}>
-          <button className="page-link" onClick={() => onPageChange(pageNumber)}>
+        <li
+          key={pageNumber}
+          className={`page-item ${currentPage === pageNumber ? "active" : ""}`}
+        >
+          <button
+            className="page-link"
+            onClick={() => onPageChange(pageNumber)}
+          >
             {pageNumber}
           </button>
         </li>
@@ -21,7 +36,7 @@ const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => 
       items.push(
         <li key="first" className="page-item">
           <button className="page-link" onClick={() => onPageChange(1)}>
-            {'<<'}
+            {"<<"}
           </button>
         </li>
       );
@@ -33,22 +48,30 @@ const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => 
         addPageNumber(i);
       }
     } else {
-      for (let i = Math.max(1, currentPage - 2); i <= Math.min(currentPage + 2, totalPages); i++) {
+      for (
+        let i = Math.max(1, currentPage - 2);
+        i <= Math.min(currentPage + 2, totalPages);
+        i++
+      ) {
         addPageNumber(i);
       }
       if (currentPage > 3) {
-        items.splice(1, 0, (
+        items.splice(
+          1,
+          0,
           <li key="separator-start" className="page-item disabled">
             <span className="page-link">...</span>
           </li>
-        ));
+        );
       }
       if (currentPage < totalPages - 2) {
-        items.splice(items.length - 1, 0, (
+        items.splice(
+          items.length,
+          0,
           <li key="separator-end" className="page-item disabled">
             <span className="page-link">...</span>
           </li>
-        ));
+        );
       }
     }
 
@@ -56,7 +79,10 @@ const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => 
     if (currentPage !== totalPages && totalPages > 1) {
       items.push(
         <li key="last" className="page-item">
-          <button className="page-link" onClick={() => onPageChange(totalPages)}>
+          <button
+            className="page-link"
+            onClick={() => onPageChange(totalPages)}
+          >
             {'>>'}
           </button>
         </li>
@@ -65,19 +91,43 @@ const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => 
 
     return items;
   };
-
+  const pageLength = [{ title: 10 }, { title: 25 }, { title: 50 }];
+  const handlePageRowLenght = (value) => {
+    console.log("handlePageRowLenght", value);
+    setRowsPerPage(value.title)
+  };
   return (
-    <nav>
+    <nav className="flex items-center justify-between">
+      <div className="flex items-center ">
+        <p className="me-3">show</p>
+        <SelectDropDown
+          listItem={pageLength}
+          title={"title"}
+          selectedOption={rowsPerPage}
+          onClickHandler={handlePageRowLenght}
+        />
+        <p className="ms-3">entries</p>
+      </div>
       <ul className="flex justify-end	">
-        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-          <button className="page-link" onClick={() => onPageChange(currentPage - 1)}>
+        <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+          <button
+            className="page-link"
+            onClick={() => onPageChange(currentPage - 1)}
+          >
             <img src={LeftArrow} />
           </button>
         </li>
         {renderPaginationItems()}
-        <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-          <button className="page-link" onClick={() => onPageChange(currentPage + 1)}>
-            Next
+        <li
+          className={`page-item ${
+            currentPage === totalPages ? "disabled" : ""
+          }`}
+        >
+          <button
+            className="page-link"
+            onClick={() => onPageChange(currentPage + 1)}
+          >
+            <img src={RightArrow} />
           </button>
         </li>
       </ul>
