f094386e5e66bc1b8522ca53205dc36f06859a3d
SumeshMerchant <sumesh@merchantech.com>
2024-07-17 19:20:12 +0530

    resolve system access and item sales report

--- src/hooks/priceFormate.js
+++ src/hooks/priceFormate.js
@@ -1,11 +1,12 @@
 export function priceFormate(input) {
-    let string = input.toString();
-    let parts = string.split('.');
-    let integerPart = parts[0];
-    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    if (parts.length > 1) {
-      return integerPart + '.' + parts[1];
-    } else {
-      return integerPart;
-    }
+  if (!input) return;
+  let string = input?.toString();
+  let parts = string?.split(".");
+  let integerPart = parts[0];
+  integerPart = integerPart?.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  if (parts?.length > 1) {
+    return integerPart + "." + parts[1];
+  } else {
+    return integerPart;
   }
+}
