1aeb49833ff11de5e87167744661b9bcc04b2b87
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2024-10-01 14:46:55 +0500

    Added Filter and Measures component

--- src/Components/Reporting/InventoryReport/InventoryTable.js
+++ src/Components/Reporting/InventoryReport/InventoryTable.js
@@ -19,43 +19,13 @@ import PasswordShow from "../../../Common/passwordShow";
 import NoDataFound from "../../../reuseableComponents/NoDataFound";
 import useDelayedNodata from "../../../hooks/useDelayedNoData";
 import plusIcon from "../../../Assests/Products/plusIcon.svg";
-<<<<<<< HEAD
+import Button from '@mui/material/Button';
+import InventoryTableColumns from "./InventoryTableColumns";
+
 import InventoryFilter from "./InventoryFilter";
 import InventoryMeasures from "./InventoryMeasures";
 
-const StyledTable = styled(Table)(({ theme }) => ({
-  padding: 2, // Adjust padding as needed
-}));
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#253338",
-    color: theme.palette.common.white,
-    fontFamily: "CircularSTDMedium !important",
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-    fontFamily: "CircularSTDBook !important",
-  },
-  [`&.${tableCellClasses.table}`]: {
-    fontSize: 14,
-    fontFamily: "CircularSTDBook !important",
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  "&:last-child td, &:last-child th": {},
-  "& td, & th": {
-    border: "none",
-  },
-}));
-=======
-import Button from '@mui/material/Button';
-import InventoryTableColumns from "./InventoryTableColumns";
 const emails = ['username@gmail.com', 'user02@gmail.com'];
->>>>>>> 47cbef1dae20a325e23e5aca4d6731e1eaec131d
 
 const InventoryTable = (props) => {
 
@@ -167,17 +137,8 @@ const InventoryTable = (props) => {
                   <td>$20.10458</td>
                   <td>$200.458</td>
                   <td></td>
-<<<<<<< HEAD
 
-                </tr >
-              </tbody >
-            </table >
-          </div >
-
-
-=======
-                
-              </tr>
+                </tr>
               </tbody>
               <tfoot>
                 <tr>
@@ -198,11 +159,10 @@ const InventoryTable = (props) => {
 
                 </tr>
               </tfoot>
-          </table>
-        </div>
-        
-            
->>>>>>> 47cbef1dae20a325e23e5aca4d6731e1eaec131d
+            </table>
+          </div>
+
+
         </Grid >
       </Grid >
     </>
